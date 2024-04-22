03eb2780888a52dc2af7e632369d3d22499ad759
ThienPPham <90206572+ThienPPham@users.noreply.github.com>
2024-04-22 13:35:00 +0700

    json-server

---
+++ json-server/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Count from './components/Count';
+import Input from './components/Input';
+import Show from './components/Show';
+import TodoApp from './components/ToDoList';
+import SearchFilter from './components/Search';
+import ChangeColor from './components/ChangeColor';
+
+function App() {
+  const data = ['Apple', 'Banana', 'Orange', 'Pear', 'Pineapple'];
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Count />} />
+          <Route path='/input' element={<Input />} />
+          <Route path='/show' element={<Show />} />
+          <Route path='/todolist' element={<TodoApp />} />
+          <Route path='/search' element={
+            <>
+              <h1>Search Filter Example</h1>
+              <SearchFilter data={data} />
+            </>
+          } />
+          <Route path='/changeColor' element={<ChangeColor/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+export default App;
